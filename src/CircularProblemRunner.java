4a8b6eeb554fa5d41fa10d380678c6de317b55dd
BT_4N2_13 <yhuang6817@bths.edu>
2022-10-28 12:33:52 -0400

    Worked on Circular problem

---
+++ src/CircularProblemRunner.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class CircularProblemRunner {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Please enter the amount of numbers: ");
+        CircularProblem x;
+    }
+}
