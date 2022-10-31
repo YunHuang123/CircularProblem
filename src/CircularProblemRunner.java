19ad4c5a6027d6707d44d526412e3afe51618aea
YunHuang123 <114007330+YunHuang123@users.noreply.github.com>
2022-10-31 17:19:21 -0400

    Finished CircularProblem

--- src/CircularProblemRunner.java
+++ src/CircularProblemRunner.java
@@ -1,8 +1,14 @@
 import java.util.Scanner;
 public class CircularProblemRunner {
     public static void main(String[] args) {
+
+        CircularProblem num = new CircularProblem();
         Scanner s = new Scanner(System.in);
-        System.out.print("Please enter the amount of numbers: ");
-        CircularProblem x;
+        System.out.println("Welcome to the Circular Problem Solver!");
+        System.out.print("How many numbers are in the circle: ");
+        int n = s.nextInt();
+        int solution = num.circular(n);
+        System.out.println("The last remaining number is: " + solution);
     }
 }
+
